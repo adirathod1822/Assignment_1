feeb84d097effb7cf5735f5ce29e967b676c4860
adirathod1822 <adirathod1822@gmail.com>
2024-02-11 00:24:14 +0530

    new

--- src/Assignment_2/Fourth.java
+++ src/Assignment_2/Fourth.java
@@ -6,7 +6,6 @@ public class Fourth {
             int n = 5;
             char currentChar = 'A';
             alphaPattern(n,currentChar);
-
         }
     public static void alphaPattern(int n, char alpha){
         for (int i = 0; i < n; i++) {
@@ -26,7 +25,7 @@ public class Fourth {
 
         alpha -= 2;
 
-        // Lower half of the diamond
+
         for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
