f822b4e3ee5f4956653107e9949a2045906ffa19
adirathod1822 <adirathod1822@gmail.com>
2024-02-10 23:50:57 +0530

    new

--- src/Assignment_2/Third.java
+++ src/Assignment_2/Third.java
@@ -6,23 +6,19 @@ public class Third {
     }
     public static void design(int n){
         for (int i = 1; i <= n; i++) {
-            // print spaces
             for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
-            // print star
             for (int k = 0; k < i * 2 - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        // lower pyramid
+
         for (int i = 1; i <= n - 1; i++) {
-            // print spaces
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            // print star
             for (int k = (n-i)*2-1; k>0; k--) {
                 System.out.print("*");
             }
