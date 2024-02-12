0f57963852db6b121aaa456be5bab651ad11afc9
adirathod1822 <adirathod1822@gmail.com>
2024-02-12 16:46:28 +0530

    c

--- src/Assignment_3/First.java
+++ src/Assignment_3/First.java
@@ -2,26 +2,55 @@ package Assignment_3;
 import java.util.*;
 public class First {
     public static void main(String[] args) {
-        Scanner ip = new Scanner(System.in);
-        int row = ip.nextInt();
-        for (int i = 0; i < row; i++) {
-            for (int k = 0; k < row - i ; k++) {
-                System.out.print(" ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of operation :- ");
+        int numOp=sc.nextInt();
+        String[] ans = new String[numOp];
+        if(numOp <4) for (int i = 0; i < numOp; i++) {
+            System.out.print("Choose Operation type :");
+            int choice=sc.nextInt();
+            System.out.print("Enter the number : ");
+            int value=sc.nextInt();
+            switch (choice){
+                case 1:
+                    ans[i] = isOdd(value) ?"Odd":"Even";
+                    break;
+                case 2:
+                    ans[i] = isPrime(value) ?"Prime":"Composite";
+                    break;
+                case 3:
+                    ans[i] = isPalindrome(value) ?"Palindrome":"NotPalindrome";
+                    break;
             }
-            for (int j = 0; j < i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
         }
-        for (int i = row; i > 0; i--) {
-            for (int k = 0; k < row -i ; k++) {
-                System.out.print(" ");
-            }
-            for (int j = i; j > 0; j--) {
-                System.out.print("* ");
-            }
-            System.out.println();
+        else System.out.println("Invalid Operation!!");
+        for(String s:ans){
+            if(s!=null) System.out.println(s);
         }
+//        System.out.println(Arrays.toString(ans));
 
     }
+    public static boolean isOdd(int n){
+        return n%2==1;
+    }
+    public static boolean isPrime(int n){
+    if(n <= 1) return false;
+        for (int i = 2; i < n/2; i++) {
+            if(n%i == 0 ) return false;
+        }
+        return true;
+    }
+    public static boolean isPalindrome(int num){
+    int reversed= 0;
+    int rem = 0 ;
+    int orginal = num;
+
+    while (num > 0){
+        rem = num % 10;
+        reversed = (reversed * 10) + rem;
+        num /= 10;
+    }
+    return orginal == reversed;
+}
+
 }
