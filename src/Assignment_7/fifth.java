dbe1f4ab1681bae177b0dbad5bab49dce0ff81c8
adirathod1822 <adirathod1822@gmail.com>
2024-02-21 17:20:02 +0530

    7

--- src/Assignment_7/fifth.java
+++ src/Assignment_7/fifth.java
@@ -1,19 +1,37 @@
 package Assignment_7;
 
 public class fifth extends Thread {
+    int sleep;
+    String msg;
+    Thread t;
+    public fifth(int sleep, String msg, Thread t){
+        this.sleep=sleep;
+        this.msg = msg;
+        this.t=t;
+    }
     public void run() {
         try {
-            Thread.sleep(1000);
-            System.out.println("Thread executed after 1 second delay");
+            Thread.sleep(sleep);
+            System.out.println("Thread "+msg+" executed");
+//            if(Thread.currentThread().isAlive())System.out.println("main thread");
+//            else System.out.println("dead");
+            System.out.println(t.getName());
+            System.out.println(t.isAlive());
+            System.out.println(Thread.currentThread().getName());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        fifth thread1 = new fifth();
-        fifth thread2 = new fifth();
+        fifth thread1 = new fifth(0,"1",Thread.currentThread());
+        fifth thread2 = new fifth(1000,"2",Thread.currentThread());
         thread1.start();
         thread2.start();
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Main ended");
+//        else System.out.println("dead");
     }
 }
